570cc3efabb5d2792af0f6f6c79b5e2cee3b355e
viktorce <viktor.gjorgonoski@gmail.com>
2025-03-21 10:17:34 +0000

    update

--- upcoming/assets/js/waitlist.js
+++ upcoming/assets/js/waitlist.js
@@ -24,15 +24,26 @@ window.onclick = function(event) {
 }
 
 
-var timerId =
-    countdown(
-        new Date(2025, 2, 28, 23, 59, 59, 999),
-        function(ts) {
-            var days = ts.days;
-            var hours = String(ts.hours).padStart(2, '0');
-            var minutes = String(ts.minutes).padStart(2, '0');
-            var seconds = String(ts.seconds).padStart(2, '0');
-            var formattedTime = `${days} days<br>${hours}:${minutes}:${seconds}`;
-            document.getElementById('countdown').innerHTML = formattedTime;
-        },
-        countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+countdown(
+    new Date(2025, 4, 28, 23, 59, 59, 999),
+    function(ts) {
+        var days = ts.days;
+        var hours = String(ts.hours).padStart(2, '0');
+        var minutes = String(ts.minutes).padStart(2, '0');
+        var seconds = String(ts.seconds).padStart(2, '0');
+        var formattedTime = `${days} days`;
+        document.getElementById('countdown').innerHTML = formattedTime;
+    },
+    countdown.DAYS);
+
+countdown(
+    new Date(2025, 2, 28, 23, 59, 59, 999),
+    function(ts) {
+        var days = ts.days;
+        var hours = String(ts.hours).padStart(2, '0');
+        var minutes = String(ts.minutes).padStart(2, '0');
+        var seconds = String(ts.seconds).padStart(2, '0');
+        var formattedTime = `${days} days`;
+        document.getElementById('countdown-2').innerHTML = '-' + formattedTime;
+    },
+    countdown.DAYS);
